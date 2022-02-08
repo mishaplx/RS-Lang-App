3d4072c66c8d20416461a2b272ac21cc497aacf2
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2022-02-08 15:00:49 +0300

    fix: add component of Team

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,11 +1,13 @@
 import Description from "../Description/Description";
 import Overview from "../Overview/Overview";
+import Team from "../Team/Team";
 
 function Main() {
   return (
     <main className="main">
       <Overview />
       <Description />
+      <Team />
     </main>
   );
 }
