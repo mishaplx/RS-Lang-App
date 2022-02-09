6b55509f8d06a80b2f7a45ffc8d05b5c45cf8f2c
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2022-02-09 22:49:12 +0300

    fix: add check for registration and authorization

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -12,22 +12,58 @@ function Login() {
       },
       body: JSON.stringify(user)
     });
-    const content = await rawResponse.json();
+    const loginInput = document.querySelector('.login-input')
+    const passwordInput = document.querySelector('.password')
+    const uncorrectLogin = document.querySelector('.uncorrect-login')
+    const uncorrectPassword = document.querySelector('.uncorrect-password')
 
-    console.log(content);
-    localStorage.setItem(userLog.email, content.token)
-    localStorage.setItem(`UserName`, content.name)
-    window.location.reload()
+    if (rawResponse.ok) {
+      const content = await rawResponse.json();
+      localStorage.setItem(userLog.email, content.token)
+      localStorage.setItem(`UserName`, content.name)
+      window.location.reload()
+      uncorrectLogin.style.display = 'none'
+      uncorrectPassword.style.display = 'none'
+      if (loginInput.classList.contains('.uncorrect') === true) {
+        loginInput.classList.remove('uncorrect')
+      }
+      if (passwordInput.classList.contains('.uncorrect') === true) {
+        passwordInput.classList.remove('uncorrect')
+      }
+    } else {
+      if (rawResponse.status === 403) {
+        passwordInput.classList.add('uncorrect-input')
+        uncorrectPassword.style.display = 'block'
+        uncorrectLogin.style.display = 'none'
+        loginInput.classList.remove('uncorrect-input')
+      }
+      if (rawResponse.status === 404) {
+        loginInput.classList.add('uncorrect-input')
+        passwordInput.classList.remove('uncorrect-input')
+        uncorrectLogin.style.display = 'block'
+        uncorrectPassword.style.display = 'none'
+      }
+      console.log(`Ошибка: ${rawResponse.status}`)
+    }
   };
+
   const handleClick = (event) => {
+    const frameLogin = document.querySelector('.login-frame')
     const login = document.querySelector('.login')
     const registration = document.querySelector('.registration')
     login.classList.toggle('open');
     registration.classList.toggle('open');
+    setTimeout(() => {
+      frameLogin.style.display = 'block'
+    }, 550)
   }
   const handleClickClose = () => {
+    const frameLogin = document.querySelector('.login-frame')
     const login = document.querySelector('.login')
     login.classList.toggle("open")
+    setTimeout(() => {
+      frameLogin.style.display = 'none'
+    }, 550)
   }
   const handleShowPass = () => {
     const checbox = document.querySelector('.show__pass-input')
@@ -52,33 +88,37 @@ function Login() {
     })
   }
   return (
-    <div className="login" >
-      <img src={closeButton} alt="close" className="close__button" onClick={handleClickClose} />
-      <div className="title">
-        <img src={loginImg} alt="login" width="50px" />
-        <h3>Login</h3>
-      </div>
-      <form onSubmit={handleSubmit} method="post">
-        <input
-          type="login"
-          className="login-input"
-          placeholder="Login..."
-          value={userLog.email}
-          onChange={e => setUserLog({ ...userLog, email: e.target.value })} />
-        <input
-          type="password"
-          className="password"
-          placeholder="Password..."
-          value={userLog.password}
-          onChange={e => setUserLog({ ...userLog, password: e.target.value })} />
-        <div className="show__pass">
-          <input type="checkbox" className="show__pass-input" onClick={handleShowPass} />
-          <span>Show password</span>
+    <div className="login-frame">
+      <div className='login'>
+        <img src={closeButton} alt="close" className="close__button" onClick={handleClickClose} />
+        <div className="title">
+          <img src={loginImg} alt="login" width="50px" />
+          <h3>Login</h3>
         </div>
+        <form onSubmit={handleSubmit} method="post">
+          <input
+            type="login"
+            className="login-input"
+            placeholder="Login..."
+            value={userLog.email}
+            onChange={e => setUserLog({ ...userLog, email: e.target.value })} />
+          <p className='uncorrect-login'>Пользователя с таким email не существует</p>
+          <input
+            type="password"
+            className="password"
+            placeholder="Password..."
+            value={userLog.password}
+            onChange={e => setUserLog({ ...userLog, password: e.target.value })} />
+          <p className='uncorrect-password'>Неверный пароль</p>
+          <div className="show__pass">
+            <input type="checkbox" className="show__pass-input" onClick={handleShowPass} />
+            <span>Show password</span>
+          </div>
 
-        <input type="submit" className="submit" value="Sing In" />
-        <p onClick={handleClick}>Don't have an account? Sing Up</p>
-      </form>
+          <input type="submit" className="submit" value="Sing In" />
+          <p onClick={handleClick}>Don't have an account? Sing Up</p>
+        </form>
+      </div>
     </div>
   )
 }
--- src/component/Registration/Registration.js
+++ src/component/Registration/Registration.js
@@ -4,6 +4,11 @@ import closeButton from '../../assets/close-button.png'
 import React, { useState } from 'react'
 
 function Registration() {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
   const createUser = async user => {
     const rawResponse = await fetch('https://react-rs-language.herokuapp.com/users', {
       method: 'POST',
@@ -13,9 +18,47 @@ function Registration() {
       },
       body: JSON.stringify(user)
     });
-    const content = await rawResponse.json();
+    const loginInput = document.querySelector('.login-input')
+    const nameUncorrect = document.querySelector('.uncorrect-name')
+    const nameInput = document.querySelector('.name__user')
+    const passwordInput = document.querySelector('.passwordReg')
+    const userReg = document.querySelector('.user-register')
+    const passUncorrect = document.querySelector('.uncorrect-pass')
+    if (rawResponse.ok) {
+      const content = await rawResponse.json();
+      console.log(content);
+      loginInput.classList.remove('uncorrect-inputReg')
+      passwordInput.classList.remove('uncorrect-inputReg')
+      nameInput.classList.remove('uncorrect-inputReg')
+      nameUncorrect.style.display = 'none'
+      userReg.style.display = 'none'
+      passUncorrect.style.display = 'none'
+    } else {
+      if (rawResponse.status === 417) {
+        userReg.style.display = 'block'
+        loginInput.classList.add('uncorrect-inputReg')
+        passwordInput.classList.remove('uncorrect-inputReg')
+        nameInput.classList.remove('uncorrect-inputReg')
+        nameUncorrect.style.display = 'none'
+        passUncorrect.style.display = 'none'
+      }
+      if (rawResponse.status === 422) {
+        console.log(user.password)
+        if (user.password.length >= 8) {
+          nameInput.classList.add('uncorrect-inputReg')
+          nameUncorrect.style.display = 'block'
+        } else {
+          passwordInput.classList.add('uncorrect-inputReg')
+          nameInput.classList.remove('uncorrect-inputReg')
+          loginInput.classList.remove('uncorrect-inputReg')
+          nameUncorrect.style.display = 'none'
+          userReg.style.display = 'none'
+          passUncorrect.style.display = 'block'
+        }
+      }
+      console.log(`Ошибка: ${rawResponse.status}`)
+    }
 
-    console.log(content);
   };
   const handleClickReg = (event) => {
     const login = document.querySelector('.login')
@@ -24,8 +67,12 @@ function Registration() {
     login.className = 'login open'
   }
   const handleClickClose = () => {
+    const frameLog = document.querySelector('.login-frame')
     const registration = document.querySelector('.registration')
     registration.classList.toggle("open")
+    setTimeout(() => {
+      frameLog.style.display = 'none'
+    }, 550)
   }
   const handleShowPassReg = () => {
     const checbox = document.querySelector('.show__pass-inputReg')
@@ -37,11 +84,6 @@ function Registration() {
       password.type = 'password'
     }
   }
-  const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password: ''
-  })
   function handleSubmit(e) {
     e.preventDefault();
     createUser({ "name": user.name, "email": user.email, "password": user.password })
@@ -65,18 +107,21 @@ function Registration() {
           placeholder="Name..."
           value={user.name}
           onChange={e => setUser({ ...user, name: e.target.value })} />
+        <p className='uncorrect-name'>Введите имя</p>
         <input
           type="email"
           className="login-input"
           placeholder="example@gmail.com"
           value={user.email}
           onChange={e => setUser({ ...user, email: e.target.value })} />
+        <p className='user-register'>Пользователь с таким email уже зарегистрирован</p>
         <input
           type="password"
           className="passwordReg"
           placeholder="Password..."
           value={user.password}
           onChange={e => setUser({ ...user, password: e.target.value })} />
+        <p className='uncorrect-pass'>Пароль должен содержать не менее 8 символов</p>
         <div className="show__pass">
           <input type="checkbox" className="show__pass-inputReg" onClick={handleShowPassReg} />
           <span>Show password</span>
