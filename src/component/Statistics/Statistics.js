99cb9054a89a7092f356e1743a4af7cb6c6558d3
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2022-02-09 23:45:46 +0300

    add: add statistics page

--- src/component/Statistics/Statistics.js
+++ src/component/Statistics/Statistics.js
@@ -1,8 +1,40 @@
 
 function Statistic() {
   return (
+    
     <main className="main">
-      <h1>Statistics</h1>
+      <div className="today-statistic">
+        <h2> Статистика за сегодня</h2>
+        <div className="total-statistics">
+          <div className="words-learned">
+            <p className="words-learned-count">0</p>
+            <p className="words-learned-title">Слов изучено</p>
+          </div>
+          <div className="right-answers">
+            <p className="right-answers-count">0%</p>
+            <p className="right-answers-title">Правильных ответов</p>
+          </div>
+
+          <div className="games-statistics">
+          <h2>Статистика игр</h2>
+            <div className="sprint-statisic">
+              <h3>Спринт</h3>
+              <p>Изучено слов : 0</p>
+              <p>Правильных ответов (%) : 0</p>
+              <p>Лучшая серия : 0</p>
+            </div>
+
+            <div className="sprint-statisic">
+              <h3>Аудиовызов</h3>
+              <p>Изучено слов : 0</p>
+              <p>Правильных ответов (%) : 0</p>
+              <p>Лучшая серия : 0</p>
+            </div>
+          </div>
+        </div>
+
+                
+      </div>
     </main>
   );
 }
