31e78cb5bdfbe2b60da709f8636606b570116dfb
mishaplx <olegorlov100oleg@gmail.com>
2022-02-16 21:25:50 +0300

    fix: add menu adaptive

--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -1,28 +1,55 @@
-import './Navigation.css'
-import {Link} from "react-router-dom";
-import imgMenu from '../../assets/menu.svg'
-import imgBook from '../../assets/book.svg'
-import imgGame from '../../assets/game.svg'
-import imgStat from '../../assets/stats.svg'
+import "./Navigation.css";
+import { Link } from "react-router-dom";
+import imgMenu from "../../assets/menu.svg";
+import imgBook from "../../assets/book.svg";
+import imgGame from "../../assets/game.svg";
+import imgStat from "../../assets/stats.svg";
 
 function Navigation() {
+  function handleClick(event) {
+    const navli = document.querySelectorAll(".title_nav");
+    const nav = document.querySelector(".nav");
+    const menuImg = document.querySelector(".menuImg");
+    menuImg.classList.toggle("rotate");
+    nav.classList.toggle("hide-nav");
+    for (let i = 0; i < navli.length; i++) {
+      navli[i].classList.toggle("hide");
+    }
+  }
   return (
     <nav className="nav">
       <ul>
         <li>
-          <Link to="/" className="nav__name"> <span className="nav__icon"><img src={imgMenu} width="20px" alt="menu" /></span> <p>Меню</p></Link>
+          <Link to="/" className="nav__name">
+            <span className="nav__icon" onClick={handleClick}>
+              <img src={imgMenu} className="menuImg" width="20px" alt="menu" />
+            </span>
+            <p className="title_nav">Меню</p>
+          </Link>
         </li>
         <li>
-        
-        <Link to="/textbook" className="nav__name"><span className="nav__icon"><img src={imgBook} width="20px" alt="menu" /></span> <p>Электронный учебник</p></Link>
+          <Link to="/textbook" className="nav__name">
+            <span className="nav__icon">
+              <img src={imgBook} width="20px" alt="menu" />
+            </span>
+            <p className="title_nav">Электронный учебник</p>
+          </Link>
         </li>
-        <li>  
-          
-          <Link to="/minigames" className="nav__name"> <span className="nav__icon"><img src={imgGame} width="20px" alt="menu" /></span><p>Мини-игры</p> </Link>
+        <li>
+          <Link to="/minigames" className="nav__name">
+            <span className="nav__icon">
+              <img src={imgGame} width="20px" alt="menu" />
+            </span>
+            <p className="title_nav">Мини-игры</p>
+          </Link>
         </li>
-        <li> 
-       
-          <Link to="/statistics" className="nav__name">    <span className="nav__icon"><img src={imgStat} width="20px" alt="menu" /></span><p>Статистика</p></Link>
+        <li>
+          <Link to="/statistics" className="nav__name">
+            <span className="nav__icon">
+              <img src={imgStat} width="20px" alt="menu" />
+            </span>
+            <p className="title_nav">Статистика</p>
+          </Link>
         </li>
       </ul>
     </nav>
