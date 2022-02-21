753a164f3a07be19f423da6379eeff7031cf255f
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2022-02-21 20:54:57 +0300

    fix: add reducers

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import { sprintReducer } from './sprintReducer';
 import { audioCallReducer } from './audioCallReducer';
+import { answeredReducer } from './answeredReducer';
+import { answeredCorrectReducer } from './answeredCorrectReducer'
 
 export const rootReducer = combineReducers({
 
     sprintReducer,
-    audioCallReducer
+    audioCallReducer,
+    answeredReducer,
+    answeredCorrectReducer
 
 });
