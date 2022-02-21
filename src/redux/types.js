50208e062bd0ea3f7fdc2a3930e7764ed4e9bdad
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2022-02-21 20:50:27 +0300

    fix: add type

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,2 +1,4 @@
 export const ADD_WORD = 'ADD_WORD';
 export const ADD_WORD_COUNT = 'ADD_WORD_COUNT';
+export const ANSWERED_COUNT = 'ANSWERED_COUNT';
+export const ANSWERED_CORRECT = 'ANSWERED_CORRECT';
