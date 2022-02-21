bda5f0451483121ff4ac5f6783c0f5ad9554661c
KISLY74 <85252629+KISLY74@users.noreply.github.com>
2022-02-21 20:52:17 +0300

    fix: add functions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,7 @@
 import { ADD_WORD } from "./types";
 import { ADD_WORD_COUNT } from "./types";
+import { ANSWERED_COUNT } from "./types";
+import { ANSWERED_CORRECT } from "./types";
 
 export function incrementWords() {
 
@@ -12,5 +14,17 @@ export function incrementWords() {
 export function incrementWordsA() {
     return {
         type: ADD_WORD_COUNT
+    }
+}
+
+export function incrementAnswered() {
+    return {
+        type: ANSWERED_COUNT
+    }
+}
+
+export function incrementAnsweredCorrect() {
+    return {
+        type: ANSWERED_CORRECT
     }
 }
